fff2a575fbd173591760259ab09cfd8ce6d1845f
Daniel Freydl <daniel.freydl@tdk-electronics.tdk.com>
2019-09-30 14:29:30 +0200

    Add task for copying file to directory

---
+++ src/Metetron.FileParser/FileTasks/CopyTask.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Metetron.FileParser.FileTasks
+{
+    internal class CopyTask
+    {
+        /// <summary>
+        /// Copy a file to a directory
+        /// </summary>
+        /// <param name="filePath">The path of the file to copy</param>
+        /// <param name="directoryPath">The directory to copy the file to</param>
+        public static async Task CopyFileToDirectory(string filePath, string directoryPath)
+        {
+            CreateDirectoryIfItDoesNotExist(directoryPath);
+
+            var file = new FileInfo(filePath);
+
+            if (!IsFileAccessible(file))
+                await Task.Delay(1000);
+
+            //Do not catch exceptions, so that the task will be retried by HangFire
+            file.CopyTo($"{directoryPath}\\{file.Name}", true);
+        }
+
+        /// <summary>
+        /// Check if the given directory exist, if not create it
+        /// </summary>
+        /// <param name="directoryPath">The path to the directory</param>
+        private static void CreateDirectoryIfItDoesNotExist(string directoryPath)
+        {
+            var doesExist = Directory.Exists(directoryPath);
+
+            if (!doesExist)
+                Directory.CreateDirectory(directoryPath);
+        }
+
+        /// <summary>
+        /// Check whether the file is accessible for copying ot not
+        /// </summary>
+        /// <param name="file">The file to check</param>
+        /// <returns>True if the file can be copied, false if it is locked</returns>
+        private static bool IsFileAccessible(FileInfo file)
+        {
+            try
+            {
+                using (file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
