3a8f509ed1afb7a50b2d53ef75333375d7aabb1e
Daniel Freydl <daniel.freydl@tdk-electronics.tdk.com>
2019-10-07 18:04:58 +0200

    change FileInfos and DirectoryInfos to their abstractions

--- src/Metetron.FileParser/FileCreatedWatcher/FileCreatedWatcherTask.cs
+++ src/Metetron.FileParser/FileCreatedWatcher/FileCreatedWatcherTask.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Hangfire;
 using Metetron.FileParser.Abstractions;
@@ -16,16 +18,18 @@ namespace Metetron.FileParser.FileCreatedWatcher
     {
         private readonly ILogger<FileCreatedWatcherTask<T>> _logger;
         private readonly IWatcherDataRepository _watcherRepository;
+        private readonly IFileSystem _fileSystem;
         private readonly WatcherOptions _options;
 
         private readonly Regex _filePattern;
         private readonly Regex _subDirectoryPattern;
 
-        public FileCreatedWatcherTask(ILogger<FileCreatedWatcherTask<T>> logger, IWatcherDataRepository watcherRepository, WatcherOptions options)
+        public FileCreatedWatcherTask(ILogger<FileCreatedWatcherTask<T>> logger, IWatcherDataRepository watcherRepository, IFileSystem fileSystem, WatcherOptions options)
         {
-            this._logger = logger;
-            this._watcherRepository = watcherRepository;
-            this._options = options;
+            _logger = logger;
+            _watcherRepository = watcherRepository;
+            _fileSystem = fileSystem;
+            _options = options;
 
             _filePattern = new Regex(options.FileSearchPattern);
             _subDirectoryPattern = new Regex(options.SubDirectorySearchPattern);
@@ -35,7 +39,7 @@ namespace Metetron.FileParser.FileCreatedWatcher
         /// Check the directories for new files
         /// </summary>
         /// <returns>A list of new files, that have not been parsed yet</returns>
-        private async Task<IEnumerable<FileInfo>> CheckForNewFiles()
+        private async Task<IEnumerable<IFileInfo>> CheckForNewFiles()
         {
             _logger.LogInformation("{ParserName}: Checking whether there are new files or not...", _options.ParserName);
 
@@ -49,12 +53,12 @@ namespace Metetron.FileParser.FileCreatedWatcher
         /// Get the new files from the directories that are watchec
         /// </summary>
         /// <param name="lastFileCreationInTicks">The UTC creation time of the last matching file in ticks</param>
-        /// <returns>A list of new files, that have not been parsed yet.</returns>
-        private IEnumerable<FileInfo> GetNewFiles(long lastFileCreationInTicks)
+        /// <returns>A list of filesInfos, that have not been parsed yet.</returns>
+        private IEnumerable<IFileInfo> GetNewFiles(long lastFileCreationInTicks)
         {
-            var newFiles = new List<FileInfo>();
+            var newFiles = new List<IFileInfo>();
 
-            var dirInfo = new DirectoryInfo(_options.DirectoryToWatch);
+            var dirInfo = _fileSystem.DirectoryInfo.FromDirectoryName(_options.DirectoryToWatch);
 
             newFiles.AddRange(GetNewFilesFromSubdirectories(lastFileCreationInTicks));
 
@@ -68,18 +72,18 @@ namespace Metetron.FileParser.FileCreatedWatcher
         /// </summary>
         /// <param name="lastFileCreationInTicks">The UTC creation time of the last matching file in ticks</param>
         /// <returns>A list of new files that were found in the subdirectories</returns>
-        private IEnumerable<FileInfo> GetNewFilesFromSubdirectories(long lastFileCreationInTicks)
+        private IEnumerable<IFileInfo> GetNewFilesFromSubdirectories(long lastFileCreationInTicks)
         {
             if (string.IsNullOrWhiteSpace(_options.SubDirectorySearchPattern))
-                return Array.Empty<FileInfo>();
+                return Array.Empty<IFileInfo>();
 
             var filePattern = new Regex(_options.FileSearchPattern);
             var subDirectoryPattern = new Regex(_options.SubDirectorySearchPattern);
-            var subDirectories = new DirectoryInfo(_options.DirectoryToWatch)
+            var subDirectories = _fileSystem.DirectoryInfo.FromDirectoryName(_options.DirectoryToWatch)
                 .GetDirectories()
                 .Where(sd => subDirectoryPattern.IsMatch(sd.Name));
 
-            var newFiles = new List<FileInfo>();
+            var newFiles = new List<IFileInfo>();
 
             foreach (var subDirectory in subDirectories)
             {
@@ -96,12 +100,14 @@ namespace Metetron.FileParser.FileCreatedWatcher
         /// </summary>
         /// <param name="lastFileCreationInTicks">The UTC creation time of the last matching file in ticks</param>
         /// <returns>A list of new files that were found in the main directory</returns>
-        private IEnumerable<FileInfo> GetNewFilesFromMainDirectory(long lastFileCreationInTicks)
+        private IEnumerable<IFileInfo> GetNewFilesFromMainDirectory(long lastFileCreationInTicks)
         {
             if (!_options.CheckMainDirectory)
-                return Array.Empty<FileInfo>();
+                return Array.Empty<IFileInfo>();
 
-            return GetFilesFromDirectory(new DirectoryInfo(_options.DirectoryToWatch), lastFileCreationInTicks);
+            var directory = _fileSystem.DirectoryInfo.FromDirectoryName(_options.DirectoryToWatch);
+
+            return GetFilesFromDirectory(directory, lastFileCreationInTicks);
         }
 
         /// <summary>
@@ -110,7 +116,7 @@ namespace Metetron.FileParser.FileCreatedWatcher
         /// <param name="directory">The directory that should be checked for new files</param>
         /// <param name="lastFileCreationInTicks">The UTC creation time of the last matching file in ticks</param>
         /// <returns>A list of new files, that were found in the directory</returns>
-        private IEnumerable<FileInfo> GetFilesFromDirectory(DirectoryInfo directory, long lastFileCreationInTicks)
+        private IEnumerable<IFileInfo> GetFilesFromDirectory(IDirectoryInfo directory, long lastFileCreationInTicks)
         {
             _logger.LogDebug("{ParserName}: Checking directory \"{Directory}\" for new files...", _options.ParserName, directory.FullName);
             var allFiles = directory.GetFiles();
@@ -125,7 +131,7 @@ namespace Metetron.FileParser.FileCreatedWatcher
         /// Enqueue jobs for each file in HangFire
         /// </summary>
         /// <param name="files">The files to enqueue</param>
-        private void EnqueueFiles(IList<FileInfo> files)
+        private void EnqueueFiles(IList<IFileInfo> files)
         {
             foreach (var file in files)
             {
@@ -155,12 +161,42 @@ namespace Metetron.FileParser.FileCreatedWatcher
             if (parserData != null)
                 return parserData;
 
-            _logger.LogDebug("{ParserName}: Data not found in database. Create it from scratch...", _options.ParserName)
+            _logger.LogDebug("{ParserName}: Data not found in database. Create it from scratch...", _options.ParserName);
             parserData = new WatcherData { ParserName = _options.ParserName, LastFileCreationInTicks = DateTime.MinValue.Ticks };
 
             await _watcherRepository.InsertWatcherDataAsync(parserData);
 
             return parserData;
+        }
+
+        /// <summary>
+        /// Loop that check for new files and queues them up
+        /// </summary>
+        /// <param name="cancellationToken">Token to cancel the loop</param>
+        private void ParserLoop(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    Task.Run(async () => await CheckForNewFiles(), cancellationToken);
+                }
+                catch (Exception exception)
+                {
+                    _logger.LogError(exception, "{ParserName}: Exception occurred while checking for new files");
+                }
+                Task.Run(async () => await Task.Delay(_options.PollingInterval), cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// Used for starting the parser thread
+        /// </summary>
+        /// <param name="cancellationToken">Token to cancel the operation</param>
+        private void Start(CancellationToken cancellationToken)
+        {
+            var task = new Task(() => ParserLoop(cancellationToken), cancellationToken, TaskCreationOptions.LongRunning);
+            task.Start();
         }
     }
 }
